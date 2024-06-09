3ac29a1c93680a2b392590fb64877d169bad2849
Rajveer_Kalsi <rajveer.parveet@gmail.com>
2024-06-09 12:16:54 +0530

    prisma init

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json())
+
 app.use("/api/posts", postRoute);
+app.use("/api/auth", authRouter);
 
 app.listen(8800, ()=>{
     console.log("Server is Running");
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -2,8 +2,8 @@ import express from "express"
 
 const router = express.Router()
 
-router.get("/test", (req,res)=> {
-    console.log("router works!")
-})
+// router.get("/test", (req,res)=> {
+//     console.log("router works!")
+// })
 
 export default router;
